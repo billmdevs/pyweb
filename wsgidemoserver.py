7a10e9096e393ec67afde2aee3f679e3f499aa47
Bill Morrisson <billmorrissonjr@gmail.com>
2019-07-30 18:11:23 -0400

    Minimalistic WSGI implementation to help the server interface with other Python webframeworks

--- wsgidemoserver.py
+++ wsgidemoserver.py
@@ -60,6 +60,67 @@ class WSGIServer(object):
 	def parse_request(self, text):
 		request_line = text.splitlines()[0]
 		request_line = request_line.rstrip("\r\n")
-		# Breal down the request line into components
+		# Break down the request line into components
 		self.request_method, self.path, self.request_version = request_line.split()
+	def get_environ(self):
+		env = {}
+		# Required WSGI variables
+		env["wsgi.version"] = (1,0)
+		env["wsgi.url_scheme"] = "http"
+		env["wsgi.input"] = io.StringIO(self.request_data)
+		env["wsgi.errors"] = sys.stderr
+		env["wsgi.multithread"] = False
+		env["wsgi.multiprocess"] = False
+		env["wsgi.run_once"] = False
+		# Required CGI variables
+		env["REQUEST_METHOD"] = self.request_method	#GET
+		env["PATH_INFO"] = self.path	# /hello
+		env["SERVER_NAME"] = self.server_name # localhost
+		env["SERVER_PORT"] = str(self.server_port) # 7777
+		return env
 
+	def start_response(self, status, response_headers, exc_info=None)
+		# Add necessary server headers
+		server_headers = [
+			("Date", "Mon, 29 Jul 2019 5:39:53 GMT"),
+			("Server", "WSGIServer 0.2"),
+		]
+		self.headers_set = [status, response_headers + server_headers]
+		# Normally to adhere to WSGI specification the start_response must return a "write" callable but we'll ignore that detail for now.
+		# return self.finish_response
+	
+	def finish_response(self, result):
+		try:
+			status, response_headers = self.headers_set
+			response = f"HTTP/1.1 {status}\r\n"
+			for header in response_headers:
+				response += "{0}: {1}\r\n".format(*header)
+			response += "\r\n"
+			for data in result:
+				response += data.decode("utf-8")
+			# Print formatted response data like "curl -v"
+			print("".join(
+				f"> {line}\n" for line in response.splitlines()
+			))
+			response_bytes = response.encode()
+			self.client_connection.sendall(response_bytes)
+		finally:
+			self.client_connection.close()
+
+SERVER_ADDRESS = (HOST, PORT) = "", 7777
+
+def make_server(server_address, application):
+	server = WSGIServer(server_address)
+	server.set_app(application)
+	return server
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		sys.exit("Provide a WSGI application object:callable")
+	app_path = sys.argv[1]
+	module, application = app_path.split(":")
+	module = __import__(module)
+	application = getattr(module, application)
+	httpd = make_server(SERVER_ADDRESS, application)
+	print(f"WSGIServer: Serving HTTP on port {PORT} ...\n")
+	httpd.serve_forever()
