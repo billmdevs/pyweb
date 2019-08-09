8cc8e06afdef0fff2cfe24565370c78ff041abe5
Bill Morrisson <billmorrissonjr@gmail.com>
2019-08-08 21:38:54 -0400

    Added a little concurrency

--- demowebserver.py
+++ demowebserver.py
@@ -1,16 +1,48 @@
+import os
+import time
 import socket
 
-HOST, PORT = "", 7777
+SERVER_ADDRESS = (HOST, PORT) = "", 7777
+REQUEST_SIZE_QUEUE = 5
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print(f"Serving HTTP on port {PORT} ...")
-while True:
-	client_connection, client_address = listen_socket.accept()
-	request_data = client_connection.recv(1024)
-	print(request_data.decode("utf-8"))
+def handle_request(client_connection):
+	request = client_connection.recv(1024)
+	print(
+		"Child PID: {pid}. Parent PID {ppid}".format(
+			pid=os.getpid(),
+			ppid=os.getppid()
+		)
+	)
+	print(request.decode())
+	http_response = b"""\
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
+	client_connection.sendall(http_response)
+	time.sleep(60)
+
+
+def serve_forever():
+	listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	listen_socket.bind(SERVER_ADDRESS)
+	listen_socket.listen(REQUEST_SIZE_QUEUE)
+	print("Serving HTTP on port {PORT} ...")
+	print("Parent PID (PPID): {pid}\n".format(pid=os.getpid()))
+	while True:
+		client_connection, client_address = listen_socket.accept()
+		pid = os.fork()
+		if pid == 0: # child process
+			listen_socket.close()
+			handle_request(client_connection)
+			client_connection.close()
+			os._exit(0)
+		else: # parent process
+			client_connection.close() # close parent copy and loop over
+
+if __name__ == "__main__":
+	serve_forever()
 
 	http_response = b"""HTTP/1.1 200 OK
 
